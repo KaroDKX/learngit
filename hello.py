1a8a35c11fbd3f2ba724fdf36171e0a302b38747
KaroDong <dkx_hust@icloud.com>
2017-11-08 22:22:47 +0800

    delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-class Programmer(object):
-	def __init__(self, name, age):
-		self.name = name		
-		self.age = age
-		print("创建了一个类!")
-if __name__ == '__main__':
-	p = Programmer('albert', 25)
-	print(p)
