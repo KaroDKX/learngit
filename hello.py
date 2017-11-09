325e617ec1182b46e1aae70b21de68ea1d8367f9
KaroDong <dkx_hust@icloud.com>
2017-11-09 22:45:25 +0800

    a little py program

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-Karo:最近墙的比较厉害
+import turtle
+import time
+turtle.pensize(2)
+turtle.bgcolor("black")
+colors = ["red", "yellow", "purple", "blue"]
+turtle.tracer(False)
+for x in range(400):
+	turtle.forward(2*x)
+	turtle.color(colors[x%4])
+	turtle.left(91)
+turtle.tracer(True)
