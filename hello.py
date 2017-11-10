b36239fd8c82bd8ae78426ac82609a2c237263c8
KaroDong <dkx_hust@icloud.com>
2017-11-10 15:40:08 +0800

    add a hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
-import turtle
-import time
-turtle.pensize(2)
-turtle.bgcolor("black")
-colors = ["red", "yellow", "purple", "blue"]
-turtle.tracer(False)
-for x in range(400):
-	turtle.forward(2*x)
-	turtle.color(colors[x%4])
-	turtle.left(91)
-turtle.tracer(True)
+#TempConvert.py
+val = input("请输入待温度表示符号的温度值（例如：32C）：")
+if val[-1] in ['c', 'C']:
+	f = 1.8 * float(val[0:-1]) + 32
+	print("转换后的温度为：%.2fF"%f)
+elif val[-1] in ['f', "F"]:
+	f = (float(val[0:-1]) - 32) / 1.8
+	print("转换后的温度为：%.2fC"%f)
+else:
+	print("你咋不上天呢？乱输入东西。。。")
